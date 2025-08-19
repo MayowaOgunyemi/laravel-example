350cdb6435842ef7c87e87d93e0302dba6e86fa3
engrmayoronline <engrmayoronline@gmail.com>
2025-08-19 23:14:50 +0100

    feature/ added job listing

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,23 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'title' => 'Software Engineer',
+                'salary' => '100,000',
+            ],
+            [
+                'title' => 'Data Scientist',
+                'salary' => '120,000',
+            ],
+            [
+                'title' => 'Product Manager',
+                'salary' => '110,000',
+            ]
+        ]
+    ]);
 });
 
 Route::get('/contact', function () {

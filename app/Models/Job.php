6997c728d373b43347b2c31ee39496b7363057b8
engrmayoronline <engrmayoronline@gmail.com>
2025-08-22 00:10:26 +0100

    created relationship for job and employer

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+    use HasFactory;
     // This handles when the table name does not follow Laravel's naming convention.
     // If the table name is not 'jobs', specify it here.
     protected $table = 'jobs_listings';

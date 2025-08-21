41765bc3b4d6028a15627fc1bf97fad43b0d8769
engrmayoronline <engrmayoronline@gmail.com>
2025-08-21 01:26:55 +0100

    feature/ modified job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,35 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '100,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Data Scientist',
-                'salary' => '120,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Product Manager',
-                'salary' => '110,000',
-            ]
-        ];
-    }
+    protected $table = 'jobs_listings';
 
-    public static function find(int $id): array{
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }

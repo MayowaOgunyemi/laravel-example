6e37300486ecbb2b0208589c6b439b8dd14639b7
engrmayoronline <engrmayoronline@gmail.com>
2025-08-21 23:13:15 +0100

    feature/ modified job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+    // This handles when the table name does not follow Laravel's naming convention.
+    // If the table name is not 'jobs', specify it here.
     protected $table = 'jobs_listings';
 
     protected $fillable = ['title', 'salary'];

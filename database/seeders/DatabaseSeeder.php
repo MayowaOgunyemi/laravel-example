65bb8ae5cee15b87d4393b32f3799d5d59fc4fdf
engrmayoronline <engrmayoronline@gmail.com>
2025-08-24 00:15:05 +0100

    feature: added database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\JobSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +17,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Mayor',
+            'last_name' => 'Emmanuel',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            JobSeeder::class,
         ]);
     }
 }
